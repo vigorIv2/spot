b1a3d8dd645328aa27a1ed5f8fa9ef9f8c29767e
Igor Vasilchikov <vigor.iv.2@gmail.com>
2018-02-10 02:44:14 -0800

    very first cut of api

---
+++ spot_api.py
@@ -0,0 +1,169 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request, make_response, url_for
+import time
+from flask_httpauth import HTTPBasicAuth
+
+app = Flask(__name__, static_url_path = "")
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'testuser':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+    
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+users = [
+    {
+        'id': "blah",
+        'wallet': '0x12341q42134khjsagdf2345235',
+        'created_at': '2017-01-02 00:02:03', 
+    },
+    {
+        'id': "blah2",
+        'wallet': '0x12341412134khjsagdf2345235',
+        'created_at': '2017-11-02 00:02:03', 
+    }
+]
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+def make_public_user(user):
+    new_user = {}
+    for field in user:
+        if field == 'id':
+            new_user['uri'] = url_for('create_user', id = user['id'], _external = True)
+        else:
+            if field == 'wallet':
+                new_user['wallet'] = "hidden"
+            else:
+                new_user[field] = user[field]
+    return new_user
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+    
+@app.route('/spot/api/v1.0/tasks', methods = ['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify( { 'tasks': map(make_public_task, tasks) } )
+
+@app.route('/spot/api/v1.0/users', methods = ['GET'])
+@auth.login_required
+def get_users():
+    return jsonify( { 'users': map(make_public_user, users) } )
+
+@app.route('/spot/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+@auth.login_required
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify( { 'task': make_public_task(task[0]) } )
+
+@app.route('/spot/api/v1.0/users/<string:user_id>', methods = ['GET'])
+@auth.login_required
+def get_user(user_id):
+    user = filter(lambda t: t['id'] == user_id, users)
+    if len(user) == 0:
+        abort(404)
+    return jsonify( { 'user': make_public_user(user[0]) } )
+
+
+@app.route('/spot/api/v1.0/tasks', methods = ['POST'])
+@auth.login_required
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify( { 'task': make_public_task(task) } ), 201
+
+@app.route('/spot/api/v1.0/register', methods = ['POST'])
+@auth.login_required
+def create_user():
+    if not request.json or not 'id' in request.json:
+        abort(400)
+    user = {
+        'id': request.json['id'],
+        'wallet': '0x12341q42134khjsagdf2345235',
+        'created_at': time.time(),
+        'tokens': 20
+    }
+    users.append(user)
+    return jsonify( { 'user': make_public_user(user) } ), 201
+
+@app.route('/spot/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
+@auth.login_required
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify( { 'task': make_public_task(task[0]) } )
+    
+@app.route('/spot/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
+@auth.login_required
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify( { 'result': True } )
+    
+if __name__ == '__main__':
+#    app.run(debug = True)
+    import logging, logging.config, yaml
+    logging.config.dictConfig(yaml.load(open('logging.conf')))
+    logfile    = logging.getLogger('file')
+    logconsole = logging.getLogger('console')
+    logfile.debug("Debug FILE")
+    logconsole.debug("Debug CONSOLE")
+
+    app.run(host="0.0.0.0")
