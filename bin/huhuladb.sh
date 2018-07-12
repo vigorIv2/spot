213424c8f5add4899f666bef04c038384edaca67
Igor Vasilchikov <vigor.iv.2@gmail.com>
2018-07-12 06:38:56 +0000

    cockroachdb startup script for all nodes

---
+++ bin/huhuladb.sh
@@ -0,0 +1,11 @@
+hn=$(hostname)
+master=huhuladb00
+eo=""
+if [ "$hn" == "$master" ]; then
+	echo "Starting master $hn"
+else	
+	echo "Starting slave $hn to connect to $master"
+	eo="--join=$master:26257"
+fi
+nohup cockroach start --certs-dir=certs --host=$hn --http-host=$hn --store=$hn --port=26258 --http-port=8081 $eo > nohup_$hn.out &
+
