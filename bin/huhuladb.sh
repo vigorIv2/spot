16921ba4ba23cf9f243e53bf9efe497d0ec10892
Igor Vasilchikov <vigor.iv.2@gmail.com>
2018-07-17 06:51:29 +0000

    implemented supervisor config for cockroachdb

--- bin/huhuladb.sh
+++ bin/huhuladb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 hn=$(hostname)
 master=huhuladb00
 rte=/mnt/vol1
@@ -9,5 +10,5 @@ else
 	echo "Starting slave $hn to connect to $master"
 	eo="--join=$master:26257"
 fi
-nohup cockroach start --certs-dir=certs --host=$hn --http-host=$hn --store=$hn --port=26257 --http-port=8080 $eo >> nohup_$hn.out &
+cockroach start --certs-dir=certs --host=$hn --http-host=$hn --store=$hn --port=26257 --http-port=8080 $eo 
 
