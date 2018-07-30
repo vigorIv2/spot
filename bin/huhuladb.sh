9db9d7ad2e68fb6480d6267868f0339001d7a4a7
Igor Vasilchikov <vigor.iv.2@gmail.com>
2018-07-30 23:53:37 +0000

    active-active cockroachdb server enabled

--- bin/huhuladb.sh
+++ bin/huhuladb.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 hn=$(hostname)
-master=huhuladb00
 rte=/mnt/vol1
 cd $rte
 eo=""
-if [ "$hn" == "$master" ]; then
-	echo "Starting master $hn"
-else	
-	echo "Starting slave $hn to connect to $master"
-	eo="--join=$master:26257"
-fi
+eo="--join=huhuladb00:26257,huhuladb01:26257,huhuladb02:26257"
+echo "Starting active-active $hn to connect to $eo"
+
 cockroach start --certs-dir=certs --host=$hn --http-host=$hn --store=$hn --port=26257 --http-port=8080 $eo 
 
